c38a63c14bb6af951c1281d273937e02a7fc4611
victorlsn <victor@iuapp.com.br>
2019-01-22 14:48:44 -0200

    Initial commit

---
+++ Example/iUSDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iUSDK
+//
+//  Created by victorlsn@gmail.com on 01/22/2019.
+//  Copyright (c) 2019 victorlsn@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
